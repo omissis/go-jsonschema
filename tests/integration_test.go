3e560d5ddb4340e1df036fa3ab4bb6633bd540f7
Felix Jung <felix@sumup.com>
2022-01-18 11:25:58 +0100

    Add test case

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -80,6 +80,11 @@ func TestBooleanAsSchema(t *testing.T) {
 	testExampleFile(t, cfg, "./data/misc/boolean-as-schema.json")
 }
 
+func TestYamlStructNameFromFile(t *testing.T) {
+	cfg := basicConfig
+	testExampleFile(t, cfg, "./data/yaml/yamlStructNameFromFile.yaml")
+}
+
 func testExamples(t *testing.T, cfg generator.Config, dataDir string) {
 	fileInfos, err := ioutil.ReadDir(dataDir)
 	if err != nil {
