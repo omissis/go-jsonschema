e48159ea56fd11cc7c849369a23fa7008543212c
Claudio Beatrice <claudi0.beatric3@gmail.com>
2023-07-08 02:15:28 +0200

    fix(ci): replaced deprecated goreleaser flag in goreleaser config

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,6 @@ set -o errexit -o nounset
 export GO_VERSION=$(go version | cut -d ' ' -f 3)
 
 goreleaser check
-goreleaser release --debug --snapshot --rm-dist
+goreleaser release --debug --snapshot --clean
 
 unset GO_VERSION
--- scripts/release.sh
+++ scripts/release.sh
@@ -6,6 +6,6 @@ set -o errexit -o nounset
 export GO_VERSION=$(go version | cut -d ' ' -f 3)
 
 goreleaser check
-goreleaser --debug release --rm-dist
+goreleaser --debug release --clean
 
 unset GO_VERSION
