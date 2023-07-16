1c89119b00b65af637bf38f971c009ce212e5041
James Durand <james.durand@alumni.msoe.edu>
2023-07-16 15:33:31 -0500

    feat: Added support for `date` formatted strings
    The `date` format is used to specify that a JSON string should conform
    to the ISO-8601 standard for defining a date _without_ a time as a
    string. In this implementation a `time.Time` value is generated given
    this type of formatted string.
    
    https://json-schema.org/draft/2020-12/json-schema-validation.html#name-dates-times-and-duration
    
    Some extra work was required here to make this work since the
    `time.Time.UnmarshalJSON` implementation expects to only be given a full
    timestamps with both a date AND a time. If you gave it just "2023-01-02"
    you would get an error. To get around this a new `SerializableDate` type
    was added which knows to to properly marshal/unmarshal as a pure date.
    This breaks a bit from how the tool has worked before because this would
    now make it expected as an import in client code.
    
    The `SerializableDate` type transparently extends the `time.Time` type
    and just changes the JSON related methods.

---
+++ types/date.go
@@ -0,0 +1,40 @@
+package types
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+)
+
+var ErrDateNotJSONString = errors.New("cannot parse non-string value as a date")
+
+type SerializableDate struct {
+	time.Time
+}
+
+func (date SerializableDate) MarshalJSON() ([]byte, error) {
+	return []byte("\"" + date.Format(time.DateOnly) + "\""), nil
+}
+
+func (date *SerializableDate) UnmarshalJSON(data []byte) error {
+	stringifiedData := string(data)
+	if stringifiedData == "null" {
+		return nil
+	}
+
+	if !strings.HasPrefix(stringifiedData, "\"") || !strings.HasSuffix(stringifiedData, "\"") {
+		return ErrDateNotJSONString
+	}
+
+	dataWithoutQuotes := stringifiedData[1 : len(stringifiedData)-1]
+
+	parsedDate, err := time.Parse(time.DateOnly, dataWithoutQuotes)
+	if err != nil {
+		return fmt.Errorf("unable to parse date from JSON: %w", err)
+	}
+
+	date.Time = parsedDate
+
+	return nil
+}
