28fed12fa5847770e129a95c9950774c22a23f94
Claudio Beatrice <claudi0.beatric3@gmail.com>
2023-10-04 15:59:10 +0200

    chore: bump golangci-lint to 1.54.2

--- scripts/tools-golang.sh
+++ scripts/tools-golang.sh
@@ -9,7 +9,7 @@ GINKGO_VERSION="v2.9.2"
 GO_COVER_TREEMAP_VERSION="v1.3.0"
 GOFUMPT_VERSION="v0.5.0"
 GOIMPORTS_VERSION="v0.8.0"
-GOLANGCI_LINT_VERSION="v1.53.3"
+GOLANGCI_LINT_VERSION="v1.54.2"
 
 go install "github.com/daixiang0/gci@${GCI_VERSION}"
 go install "github.com/golangci/golangci-lint/cmd/golangci-lint@${GOLANGCI_LINT_VERSION}"
