33ec55979ff7444b6b392f6d0aaa8e4bf9e962a0
Alberto Barba <alberto.barba@live.com>
2023-11-11 14:05:40 +0000

    chore: split generate.go file

---
+++ pkg/generator/config.go
@@ -0,0 +1,22 @@
+package generator
+
+type Config struct {
+	SchemaMappings      []SchemaMapping
+	ExtraImports        bool
+	Capitalizations     []string
+	ResolveExtensions   []string
+	YAMLExtensions      []string
+	DefaultPackageName  string
+	DefaultOutputName   string
+	StructNameFromTitle bool
+	Warner              func(string)
+	Tags                []string
+	OnlyModels          bool
+}
+
+type SchemaMapping struct {
+	SchemaID    string
+	PackageName string
+	RootType    string
+	OutputName  string
+}
---
+++ pkg/generator/output.go
@@ -0,0 +1,36 @@
+package generator
+
+import (
+	"fmt"
+
+	"github.com/atombender/go-jsonschema/pkg/codegen"
+	"github.com/atombender/go-jsonschema/pkg/schemas"
+)
+
+type output struct {
+	file          *codegen.File
+	declsByName   map[string]*codegen.TypeDecl
+	declsBySchema map[*schemas.Type]*codegen.TypeDecl
+	warner        func(string)
+}
+
+func (o *output) uniqueTypeName(name string) string {
+	v, ok := o.declsByName[name]
+
+	if !ok || (ok && v.Type == nil) {
+		return name
+	}
+
+	count := 1
+
+	for {
+		suffixed := fmt.Sprintf("%s_%d", name, count)
+		if _, ok := o.declsByName[suffixed]; !ok {
+			o.warner(fmt.Sprintf(
+				"Multiple types map to the name %q; declaring duplicate as %q instead", name, suffixed))
+
+			return suffixed
+		}
+		count++
+	}
+}
