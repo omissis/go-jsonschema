ba21b6d83527ec73c452343f0de382c98b50dd81
zrma <bulbitain@gmail.com>
2023-11-18 18:20:20 +0900

    enhance splitIdentifierByCaseAndSeparators to support non-case-sensitive languages
    - Refactored the function to accurately handle languages without case distinction (e.g., Korean, Chinese) while maintaining functionality for case-sensitive languages.
    - Implemented rune-based string processing for better multi-language support.
    - Preserved original logic for case distinction where applicable.

--- tests/data/misc/capitalization/capitalization.go
+++ tests/data/misc/capitalization/capitalization.go
@@ -38,4 +38,16 @@ type Capitalization struct {
 
 	// URLSomething corresponds to the JSON schema field "url_something".
 	URLSomething *string `json:"url_something,omitempty" yaml:"url_something,omitempty" mapstructure:"url_something,omitempty"`
+
+	// Aアトリビュート corresponds to the JSON schema field "アトリビュート".
+	Aアトリビュート *string `json:"アトリビュート,omitempty" yaml:"アトリビュート,omitempty" mapstructure:"アトリビュート,omitempty"`
+
+	// A属性 corresponds to the JSON schema field "属性".
+	A属性 *string `json:"属性,omitempty" yaml:"属性,omitempty" mapstructure:"属性,omitempty"`
+
+	// A屬性 corresponds to the JSON schema field "屬性".
+	A屬性 *string `json:"屬性,omitempty" yaml:"屬性,omitempty" mapstructure:"屬性,omitempty"`
+
+	// A속성 corresponds to the JSON schema field "속성".
+	A속성 *string `json:"속성,omitempty" yaml:"속성,omitempty" mapstructure:"속성,omitempty"`
 }
