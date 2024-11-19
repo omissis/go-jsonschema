6575e69be3462ec74cbe28a37f428435eb441780
Steve Kuznetsov <stekuznetsov@microsoft.com>
2024-11-19 09:26:02 -0700

    *: allow users to ask for minimal names
    The new --minimal-names flag allows users to ask the generator to come
    up with the most minimal name possible, meaning that redundant context
    prefixes are omitted and collection type names are inferred from field
    names where possible.
    
    A real-world use-case JSON Schema is imported as a test case; some
    examples of names before and after:
    
    RolloutSpecificationOrchestratedStepsElem -> OrchestratedStep
    RolloutSpecificationOrchestratedStepsElemApplicationsApplyAcrossServiceResources
    -> ApplyAcrossServiceResources
    
    Signed-off-by: Steve Kuznetsov <stekuznetsov@microsoft.com>

--- pkg/generator/config.go
+++ pkg/generator/config.go
@@ -3,19 +3,33 @@ package generator
 import "github.com/atombender/go-jsonschema/pkg/schemas"
 
 type Config struct {
-	SchemaMappings      []SchemaMapping
-	ExtraImports        bool
-	Capitalizations     []string
-	ResolveExtensions   []string
-	YAMLExtensions      []string
-	DefaultPackageName  string
-	DefaultOutputName   string
+	SchemaMappings []SchemaMapping
+	// ExtraImports allows the generator to pull imports from outside the standard library.
+	ExtraImports bool
+	// Capitalizations configures capitalized forms for identifiers which take precedence over the default algorithm.
+	Capitalizations []string
+	// ResolveExtensions configures file extensions to use when resolving schema names.
+	ResolveExtensions []string
+	// YAMLExtensions configures the file extensions that are recognized as YAML files.
+	YAMLExtensions []string
+	// DefaultPackageName configures the package to declare files under.
+	DefaultPackageName string
+	// DefaultOutputName configures the file to write.
+	DefaultOutputName string
+	// StructNameFromTitle configures the generator to use the schema title as the generated struct name.
 	StructNameFromTitle bool
-	Warner              func(string)
-	Tags                []string
-	OnlyModels          bool
-	MinSizedInts        bool
-	Loader              schemas.Loader
+	// Warner provides a handler for warning messages.
+	Warner func(string)
+	// Tags specifies which struct tags should be generated.
+	Tags []string
+	// OnlyModels configures the generator to omit unmarshal methods, validations, anything but models.
+	OnlyModels bool
+	// MinSizedInts configures the generator to use the smallest int and uint types based on schema maximum values.
+	MinSizedInts bool
+	// MinimalNames configures the generator to use the shortest identifier names possible.
+	MinimalNames bool
+	// Loader provides a schema loader for the generator.
+	Loader schemas.Loader
 	// When DisableOmitempty is set to true,
 	// an "omitempty" tag will never be present in generated struct fields.
 	// When DisableOmitempty is set to false,
