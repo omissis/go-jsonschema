e89543671576fce2a4fd41dcee7733f85d5e3b35
ben <>
2025-04-13 22:00:20 +0200

    fix linter errors

--- pkg/types/time.go
+++ pkg/types/time.go
@@ -30,21 +30,25 @@ func (t *SerializableTime) UnmarshalJSON(data []byte) error {
 
 	timeWithoutQuotes := stringifiedData[1 : len(stringifiedData)-1]
 
-	// RFC 3339 full-time layouts:
+	// RFC 3339 full-time layouts.
 	layouts := []string{
-		"15:04:05",       // Time without offset
-		"15:04:05Z07:00", // Time with offset. works with ..Z, ..+09:00 ...
+		"15:04:05",       // Time without offset.
+		"15:04:05Z07:00", // Time with offset. Works with ..Z, ..+09:00 ...
 	}
 
 	var parsedTime time.Time
+
 	var err error
+
 	for _, layout := range layouts {
 		parsedTime, err = time.Parse(layout, timeWithoutQuotes)
 		if err == nil {
 			t.Time = parsedTime
+
 			return nil
 		}
 	}
+
 	if err != nil {
 		return fmt.Errorf("unable to parse time from JSON: %w", err)
 	}
