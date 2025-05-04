c62ea8f1e5aa5ce62409baf086d32e65c458cc4d
Paulin Todev <paulin.todev@gmail.com>
2025-05-04 16:21:11 +0100

    Add a toggle to disable "omitempty" (#282)
    Disable omitempty

--- pkg/generator/config.go
+++ pkg/generator/config.go
@@ -16,6 +16,11 @@ type Config struct {
 	OnlyModels          bool
 	MinSizedInts        bool
 	Loader              schemas.Loader
+	// When DisableOmitempty is set to true,
+	// an "omitempty" tag will never be present in generated struct fields.
+	// When DisableOmitempty is set to false,
+	// an "omitempty" tag will be present for all fields that are not required.
+	DisableOmitempty bool
 }
 
 type SchemaMapping struct {
